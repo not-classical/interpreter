53f471f7a357f28a190444c17e4b44fb3e1d23f2
sinakhalili <khalili@sfu.ca>
2020-07-19 17:05:48 -0700

    Add tokenizer
    Add the token class, along with a LexicalAnalyzer
    class which parses a line of text and returns an
    array of tokens.
    Reference: https://www.jayconrod.com/posts/37/a-simple-interpreter-from-scratch-in-python-part-1

---
+++ tokenize.py
@@ -0,0 +1,91 @@
+"""
+These are all the possible type for the interpreter to interpret
+
+We can add more as we go on
+"""
+import sys
+import re
+
+from typing import Union
+
+
+class Token:
+    """
+    This is the token class, with the current types being
+     - LETTER
+     - DIGIT
+     - SEMICOLON
+     - SPACE
+     - HASH (COMMENT)
+    """
+
+    LETTER, DIGIT, SEMICOLON, INSTRUCTION, GATE = [
+        "LETTER",
+        "DIGIT",
+        "SEMI-COLON",
+        "INSTRUCTION",
+        "GATE",
+    ]
+
+    def __init__(self, token_type: str, value: Union[str, int]):
+        """
+        For now just gates and qubits
+        """
+        self.token_type = token_type
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f"<{self.token_type} :: {self.value}>"
+
+
+class LexicalAnalyzer:
+    """
+    Lexes input to `Token` types
+    """
+
+    token_expressions = [
+        (r"[ \n\t]+", None),  # Spaces
+        (r"#[^\n]*", None),  # Regex for comments
+        (r";", Token.SEMICOLON),
+        (r"DEFGATE", Token.INSTRUCTION),
+        (r"DEFCIRCUIT", Token.INSTRUCTION),
+        (r"MEASURE", Token.GATE),
+        (r"CNOT", Token.GATE),
+        (r"NOP", Token.GATE),
+        (r"X", Token.GATE),
+        (r"H", Token.GATE),
+        (r"I", Token.GATE),
+        (r"[0-9]+", Token.DIGIT),
+    ]
+
+    def __init__(self, text: str):
+        self.text = text
+
+    def lex(self) -> [Token]:
+        """
+        Returns an array of tokens
+
+        Will exit with error if the tokens don't match
+        """
+        pos = 0
+        out = []
+        while pos < len(self.text):
+            match = None
+            for token_expression in self.token_expressions:
+                pattern, type_value = token_expression
+                regex = re.compile(pattern)
+                match = regex.match(self.text, pos)
+
+                if match:
+                    token_value = match.group(0)
+                    if token_value:
+                        token = Token(type_value, token_value)
+                        out.append(token)
+                    break
+            if match is None:
+                sys.stderr.write(f"Illegal sequence: {self.text[pos]}\n")
+                sys.exit()
+            else:
+                pos = match.end(0)
+
+        return out
