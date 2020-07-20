b01dcf6c695527e7a9f09dec219fa380810c215e
sinakhalili <khalili@sfu.ca>
2020-07-19 19:24:58 -0700

    Add placeholder CNOT

--- gates.py
+++ gates.py
@@ -12,3 +12,12 @@ class Hadamard(QuantumGate):
 
     def __repr__(self):
         return f"<Hadamard with {self.qubit}>"
+
+
+class CNOT(QuantumGate):
+    def __init__(self, qubit1: Qubit, qubit2: Qubit):
+        self.qubit1 = qubit1
+        self.qubit2 = qubit2
+
+    def __repr__(self):
+        return f"<CNOT with {self.qubit1} and {self.qubit2}>"
